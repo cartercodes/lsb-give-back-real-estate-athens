23dab2df80494e7acec0666a5f59021262c39984
Dispatch <dispatch@cartercodes.dev>
2026-04-08 21:13:06 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-give-back-real-estate-athens',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
